e649fef91063c41b91139c22d092623e2384206c
silsha fux <hallo@silsha.me>
2016-06-12 16:08:52 +0200

    Use Meter for open issues

--- jobs/github.rb
+++ jobs/github.rb
@@ -9,36 +9,21 @@ git_project = "rzl-tuwat"
 event_name = "git_issues_labeled_defects"
 
 ## the endpoint we'll be hitting
-uri = "https://api.github.com/repos/#{git_owner}/#{git_project}/issues"
+uri = "https://api.github.com/repos/#{git_owner}/#{git_project}/issues?state=all"
 
-## Create an array to hold our data points
-points = []
-
-## One hours worth of data for, seed 60 empty points (rickshaw acts funny if you don't).
-(0..60).each do |a|
-  points << { x: a, y: 0.01 }
-end
-
-## Grab the last x value
-last_x = points.last[:x]
-
-
-SCHEDULER.every '1m', :first_in => 0 do |job|
+SCHEDULER.every '2m', :first_in => 0 do |job|
     puts "Getting #{uri}"
     response = RestClient.get uri
     issues = JSON.parse(response.body, symbolize_names: true)
 
-    current_defects = issues.length
+    max = issues.length
+    open = 0
+    issues.each do |val|
+        if val[:state] == 'open'
+            open += 1
+        end
+    end
 
-    ## Drop the first point value and increment x by 1
-    points.shift
-    last_x += 1
-
-    ## Push the most recent point value
-    points << { x: last_x, y: current_defects  }
-
-    send_event(event_name, {
-            text: current_defects, points:points
-        })
+    send_event(event_name, {value: open, max:max, min: 0})
 
 end # SCHEDULER
