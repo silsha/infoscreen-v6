fe012a371eb62dbe5204d114d9e365f10d865d96
silsha fux <hallo@silsha.me>
2016-06-12 15:20:14 +0200

    Parse datapoints from xively. Show temperature history

--- jobs/xively.rb
+++ jobs/xively.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'time'
 
 SCHEDULER.every '20s', :first_in => 0 do |job|
   begin
@@ -6,18 +7,24 @@ SCHEDULER.every '20s', :first_in => 0 do |job|
     data = {}
 
     # Gets data from Xively
-    xively_data = HTTParty.get("https://api.xively.com/v2/feeds/42055",
+    xively_data = HTTParty.get("https://api.xively.com/v2/feeds/42055?duration=6hours&interval=0",
       { :headers => {'X-ApiKey' => "LEWdgav5SfWUWKyOfdjv89nXDheF1mwkthusI9wWgmAN3S3o", 'Content-Type' => 'application/json' }})
 
 
     # This code assigns the variables to incoming Xively data and stores the result in a hash
     xively_data['datastreams'].each do |n|
-        data[n['id']] = n['current_value']
+        data[n['id']] = n
     end
 
     # Sends the data to the DOM.
     data.each do |key, val|
-      send_event(key, {current: val})
+      if val['datapoints']
+        datapoints = []
+        val['datapoints'].each do |dpval|
+          datapoints.push({x: Time.parse(dpval['at']).to_i, y: dpval['value'].to_f})
+        end
+      end
+      send_event(key, {current: val['current_value'], points: datapoints, displayedValue: val['current_value']})
     end
   end
 end
